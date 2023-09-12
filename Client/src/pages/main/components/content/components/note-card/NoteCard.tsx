20b631bdd45fd0616d6d52b7222c9bc40517e690
neiwan <neiwan02@mail.ru>
2023-09-12 18:53:29 +0500

    сверстал контент к заметкам

--- Client/src/pages/main/components/content/components/note-card/NoteCard.tsx
+++ Client/src/pages/main/components/content/components/note-card/NoteCard.tsx
@@ -1,5 +1,36 @@
 import styles from './NoteCard.module.scss';
 
 export const NoteCard = () => {
-  return <div className={styles['note-card']}></div>;
+  return (
+    <div className={styles['note-card']}>
+      <div className={styles['note-card-icons']}>
+        <img
+          className={styles['note-card-icons-edit']}
+          src="public\assets\icons\edit.svg"
+          alt="edit"
+        />
+        <img
+          className={styles['note-card-icons-bookmark']}
+          src="public\assets\icons\bookmark.svg"
+          alt="edit"
+        />
+        <img
+          className={styles['note-card-icons-delete']}
+          src="public\assets\icons\delete.svg"
+          alt="edit"
+        />
+      </div>
+      <div className={styles['note-card-main']}>
+        <div className={styles['note-card-main-title']}>Название заметки</div>
+        <div className={styles['note-card-main-dates']}>
+          <div className={styles['note-card-main-dates-before']}>
+            дата изменения: 20.05.2003
+          </div>
+          <div className={styles['note-card-main-dates-after']}>
+            дата создания: 11.09.2001
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
