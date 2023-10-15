8409b75c47a1fb4d911b217194c171f433cda6a0
Koichi-hub <gogotimur2020@gmail.com>
2023-10-15 16:10:45 +0500

    feature: Написать markdown текстовой редактор

--- Client/src/pages/notes/components/content/components/note-editor/NoteEditor.tsx
+++ Client/src/pages/notes/components/content/components/note-editor/NoteEditor.tsx
@@ -1,5 +1,29 @@
+import { ChangeEvent, useState } from 'react';
 import styles from './NoteEditor.module.scss';
+import Markdown from 'marked-react';
 
 export const NoteEditor = () => {
-  return <div className={styles['container']}></div>;
+  const [value, setValue] = useState('');
+
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) =>
+    setValue(e.target.value);
+
+  return (
+    <div className={styles['note-editor']}>
+      <div className={styles['src-text-container']}>
+        <textarea
+          className={styles['src-text']}
+          cols={30}
+          rows={10}
+          placeholder="Новая заметка"
+          value={value}
+          onChange={onChange}></textarea>
+      </div>
+      <div className={styles['markdown-container']}>
+        <div className={styles['markdown']}>
+          <Markdown value={value} />
+        </div>
+      </div>
+    </div>
+  );
 };
